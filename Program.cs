de913572ab12c3e0a3148ae671dc08f5230788ad
Raul A. Costa <113145571+Raulcjo@users.noreply.github.com>
2023-11-15 23:23:05 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,30 +4,21 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//var connectionStringMySql = builder.Configuration.GetConnectionString("ConnectionMySql");
-//builder.Services.AddDbContext<InfoDbContext>(x => x.UseMySQL(connectionStringMySql));
+/*var connectionStringMySql = builder.Configuration.GetConnectionString("ConnectionMySql");
+
+builder.Services.AddDbContext<InfoDbContext>(x => x.UseMySQL(connectionStringMySql));*/
 
 var connectionStringPgSql = builder.Configuration.GetConnectionString("PostgreConn");
+
 builder.Services.AddDbContext<InfoDbContext>(context => context.UseNpgsql(connectionStringPgSql));
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAnyOrigin",
-        builder =>
-        {
-            builder.WithOrigins()
-                   .AllowAnyHeader()
-                   .AllowAnyMethod();
-        });
-});
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-app.UseCors("AllowAnyOrigin");
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
